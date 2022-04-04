a54b08c36d3653a4f8658fc625e0b5139b0adcf4
Jari Sääksi <jari.saaksi@gmail.com>
2022-04-04 13:15:42 +0300

    Vuosi parametrina All jos kaikki

--- routes/tapahtuma.js
+++ routes/tapahtuma.js
@@ -1,92 +1,105 @@
 const express = require('express');
+const { request } = require('../app');
 const router = express.Router();
 const tapahtuma = require('../models/tapahtuma_model');
 
 router.get('/:id?',
- function(request, response) {
-  if (request.params.id) {
-    tapahtuma.getById(request.params.id, function(err, dbResult) {
-      if (err) {
-        response.json(err);
-      } else {
-        //response.json(dbResult);
-        response.json(dbResult.rows[0]);
-      }
-    });
-  } else {
-    tapahtuma.getAll(function(err, dbResult) {
-      if (err) {
-        response.json(err);
-      } else {
-        response.json(dbResult.rows);
-        console.log(dbResult.rows)
-        //response.json(dbResult[0]);
-      }
-    });
-  }
-});
-
+  function (request, response) {
+    if (request.params.id) {
+      tapahtuma.getById(request.params.id, function (err, dbResult) {
+        if (err) {
+          response.json(err);
+        } else {
+          //response.json(dbResult);
+          response.json(dbResult.rows[0]);
+        }
+      });
+    } else {
+      tapahtuma.getAll(function (err, dbResult) {
+        if (err) {
+          response.json(err);
+        } else {
+          response.json(dbResult.rows);
+          console.log(dbResult.rows)
+          //response.json(dbResult[0]);
+        }
+      });
+    }
+  });
 router.get('/year/:value/:id?',
-function(request, response) {
-  console.log(request.params.value)
-  if (request.params.id) {
-    tapahtuma.getByYearById(request.params.value, request.params.id, function(err, dbResult) {
-      if (err) {
-        response.json(err);
+  function (request, response) {
+    console.log(request.params.value)
+    if (request.params.value=="all"){
+      tapahtuma.getAll(function (err, dbResult) {
+        if (err) {
+          response.json(err);
+        } else {
+          response.json(dbResult.rows);
+          console.log(dbResult.rows)
+          //response.json(dbResult[0]);
+        }
+      });
+    }
+     else {
+      console.log(request.params.value)
+      if (request.params.id) {
+        tapahtuma.getByYearById(request.params.value, request.params.id, function (err, dbResult) {
+          if (err) {
+            response.json(err);
+          } else {
+            //response.json(dbResult);
+            response.json(dbResult.rows[0]);
+          }
+        });
       } else {
-        //response.json(dbResult);
-        response.json(dbResult.rows[0]);
+        tapahtuma.getAllByYear(request.params.value, function (err, dbResult) {
+          if (err) {
+            response.json(err);
+          } else {
+            response.json(dbResult.rows);
+            //response.json(dbResult[0]);
+          }
+        });
       }
-    });
-  } else {
-    tapahtuma.getAllByYear(request.params.value, function(err, dbResult) {
-      if (err) {
-        response.json(err);
-      } else {
-        response.json(dbResult.rows);
-        //response.json(dbResult[0]);
-      }
-    });
+    }
   }
-}
-
 
 );
 
-router.post('/', 
-function(request, response) {
-  tapahtuma.add(request.body, function(err, dbResult) {
-    if (err) {
-      response.json(err);
-    } else {
-      response.json(dbResult.rows);
-    }
+router.post('/',
+  function (request, response) {
+    tapahtuma.add(request.body, function (err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult.rows);
+      }
+    });
   });
-});
 
 
-router.delete('/:id', 
-function(request, response) {
-  tapahtuma.delete(request.params.id, function(err, dbResult) {
-    if (err) {
-      response.json(err);
-    } else {
-      response.json(dbResult.rows);
-    }
+router.delete('/:id',
+  function (request, response) {
+    tapahtuma.delete(request.params.id, function (err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult.rows);
+      }
+    });
   });
-});
 
 
-router.put('/:id', 
-function(request, response) {
-  tapahtuma.update(request.params.id, request.body, function(err, dbResult) {
-    if (err) {
-      response.json(err);
-    } else {
-      response.json(dbResult.rows.affectedRows);
-    }
+router.put('/:id',
+  function (request, response) {
+    tapahtuma.update(request.params.id, request.body, function (err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult.rows.affectedRows);
+      }
+    });
   });
-});
 
 
 // router.get('vuodentapahtumat/tapahtuman_pvm'),
